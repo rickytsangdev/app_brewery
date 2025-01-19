898c93a20933ebed82af11b3cb00277e908283eb
rickytsangdev <rickytsang.dev@gmail.com>
2025-01-19 23:09:20 +0100

    bucket switch logical

---
+++ first_exercices.js
@@ -0,0 +1,22 @@
+function test () {
+    var a = "3"; 
+    var b = "8"; 
+// ****************DO NOT CHANGE THE CODE ABOVE*******************
+
+// Write your code here  
+var c = a
+a = b
+b = c
+// ****************DO NOT CHANGE THE CODE BELOW*******************
+
+console.log("a is " + a)
+console.log("b is " + b)
+}
+
+test ()
+
+// think of bucket 
+// a is sand 
+// b is water 
+// in normal life you will need an another bucket to make the switch
+
